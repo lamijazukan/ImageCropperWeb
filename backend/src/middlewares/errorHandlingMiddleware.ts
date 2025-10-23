2e42675ef96544d71c4969c24289e628af095f51
lamijazukan <zukanlamija@gmail.com>
2025-10-23 23:09:56 +0200

    feat: add AppError class and enhance error handler

--- backend/src/middlewares/errorHandlingMiddleware.ts
+++ backend/src/middlewares/errorHandlingMiddleware.ts
@@ -1,13 +1,20 @@
 import { Request, Response, NextFunction } from "express";
+import { AppError } from "../utils/AppError";
 
 export function errorHandler(
-  err: any,
+  err: Error | AppError,
   req: Request,
   res: Response,
   next: NextFunction
 ) {
   console.error(err.stack || err);
-  res
-    .status(500)
-    .json({ success: false, message: err.message || "Internal Server Error" });
+
+  const statusCode =
+    err instanceof AppError && err.statusCode ? err.statusCode : 500;
+
+  res.status(statusCode).json({
+    success: false,
+    message: err.message || "Internal Server Error",
+    ...(process.env.NODE_ENV === "development" && { stack: err.stack }),
+  });
 }
