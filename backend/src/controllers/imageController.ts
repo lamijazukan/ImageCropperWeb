c30d0fbfce5ea49bdd5af07efa458d3f9fc6c307
lamijazukan <zukanlamija@gmail.com>
2025-10-24 23:57:34 +0200

    feat: add image generate endpoint

--- backend/src/controllers/imageController.ts
+++ backend/src/controllers/imageController.ts
@@ -4,6 +4,7 @@ import asyncHandler from "express-async-handler";
 import { ImageCropRequest } from "../types/request";
 import { AppError } from "../utils/AppError";
 
+// /preview
 export const previewImage = asyncHandler(
   async (req: ImageCropRequest, res, next) => {
     if (!req.file) {
@@ -22,6 +23,33 @@ export const previewImage = asyncHandler(
       .resize({
         width: Math.floor(crop.width * 0.05),
         height: Math.floor(crop.height * 0.05),
+      })
+      .png()
+      .toBuffer();
+
+    await fs.unlink(req.file.path); // cleanup
+
+    res.set("Content-Type", "image/png");
+    res.status(200).send(croppedBuffer);
+  }
+);
+
+// /generate
+
+export const generateImage = asyncHandler(
+  async (req: ImageCropRequest, res, next) => {
+    if (!req.file) {
+      throw new AppError("No image uploaded", 400);
+    }
+
+    const { crop } = req.body;
+
+    const croppedBuffer = await sharp(req.file.path)
+      .extract({
+        left: crop.x,
+        top: crop.y,
+        width: crop.width,
+        height: crop.height,
       })
       .png()
       .toBuffer();
--- backend/src/routes/imageRoutes.ts
+++ backend/src/routes/imageRoutes.ts
@@ -1,10 +1,9 @@
 import { Router } from "express";
 import upload from "../middlewares/multerMiddleware";
 import { validateCrop } from "../middlewares/validateCropMiddleware";
-import { previewImage } from "../controllers/imageController";
+import { previewImage, generateImage } from "../controllers/imageController";
 
 const router = Router();
-
 /**
  * @swagger
  * /image/preview:
@@ -34,12 +33,55 @@ const router = Router();
  *                   y: { type: number }
  *                   width: { type: number }
  *                   height: { type: number }
+ *     responses:
+ *       200:
+ *         description: Cropped image preview in PNG format scaled to 5%
  */
 router.post(
   "/image/preview",
   upload.single("image"),
   validateCrop,
   previewImage
+);
+
+/**
+ * @swagger
+ * /image/generate:
+ *   post:
+ *     operationId: preview
+ *     tags:
+ *       - image
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - image
+ *               - crop
+ *             properties:
+ *               image:
+ *                 type: string
+ *                 format: binary
+ *                 description: HD PNG image file
+ *               crop:
+ *                 type: object
+ *                 description: Crop coordinates
+ *                 properties:
+ *                   x: { type: number }
+ *                   y: { type: number }
+ *                   width: { type: number }
+ *                   height: { type: number }
+ *     responses:
+ *       200:
+ *         description: Cropped image preview in PNG format
+ */
+router.post(
+  "/image/generate",
+  upload.single("image"),
+  validateCrop,
+  generateImage
 );
 
 export default router;
