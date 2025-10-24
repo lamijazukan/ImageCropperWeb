2780a3b5d500764087ccf2906eefba2ecf3238f3
lamijazukan <zukanlamija@gmail.com>
2025-10-24 22:59:17 +0200

    chore: update gitignore, server and swagger configs

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,7 @@
 import app from "./app";
 
-
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port http://localhost:${PORT}`);
 });
