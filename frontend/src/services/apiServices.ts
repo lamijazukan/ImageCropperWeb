d555d7124d4e6aa98c27d499774e9c1d92fad792
lamijazukan <zukanlamija@gmail.com>
2025-11-03 22:08:16 +0100

    feat: build frontend base structure - Change tab favicon - Create global MUI theme configuration - Implement apiServices for backend communication - Set up CSSBaseline and ThemeProvider

--- frontend/src/services/apiServices.ts
+++ frontend/src/services/apiServices.ts
@@ -3,15 +3,30 @@ import axios from "axios";
 // axios instance
 const api = axios.create({
   baseURL: "http://localhost:5001/api",
-  headers: {
-    "Content-Type": "application/json",
-  },
+
   withCredentials: false,
 });
 
-export const getTestMessage = async () => {
-  const response = await api.get("/");
-  return response.data; // returns "Hello from Express + Swagger + TypeScript 👋"
+// image preview
+export const previewImage = async (formData: FormData) => {
+  const response = await api.post("/image/preview", formData, {
+    responseType: "blob",
+  });
+  return response.data;
+};
+
+// generate image
+export const generateImage = async (formData: FormData) => {
+  const response = await api.post("/image/generate", formData, {
+    responseType: "blob",
+  });
+  return response.data;
+};
+
+// configure logo
+export const configureLogo = async (formData: FormData) => {
+  const response = await api.post("/config", formData);
+  return response.data;
 };
 
 export default api;
